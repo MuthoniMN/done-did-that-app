e864d5976aab98c97dc9e1c7fcbeb78760f2e633
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-06-27 12:31:08 +0300

    check for finished tasks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// creates a new day
+document.querySelector('button').addEventListener('click', () => {
+    document.querySelector('#new').style.display = "block"
+    document.querySelector('#list').style.display = "none"
+})
+
 document.querySelector('#addDate').addEventListener('click', startNewDay)
 
 function startNewDay() {
@@ -6,18 +12,38 @@ function startNewDay() {
     document.querySelector('h3').innerText = document.querySelector('#date').value
 }
 
+// creates a to-do list for today
 let today = new Date()
 
-document.querySelector('h3').innerHTML = `${today.getDate()}/${today.getMonth()}/${today.getFullYear()}`
+document.querySelector('h3').innerHTML = `${today.getDate()}-${today.getMonth()}-${today.getFullYear()}`
 
 let task = []
 
 document.querySelector('#addTask').addEventListener('click', addNewTask)
 
+let num = 1
 function addNewTask() {
-    document.querySelector('#tasks').innerHTML += `<input type="checkbox"class="task" >
-    <label>${document.querySelector('#task').value}</label>
+
+    document.querySelector('#tasks').innerHTML += `
+    <div class="inputwrapper">
+    <input type="checkbox"class="task" id="task${num}">
+    <label for="task${num}">${document.querySelector('#task').value}</label>
+    </div>
 `
 
     task.push(document.querySelector('#task').value)
+
+    document.querySelector('#task').value = ''
+    num++
 }
+
+// check if the task is done
+function getChecked() {
+    document.querySelectorAll('.inputwrapper').forEach((a) => {
+        a.addEventListener('click', () => {
+            a.classList.toggle('done')
+        })
+})    
+}
+
+setInterval(getChecked, 100)
